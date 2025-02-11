2d6587316865c6670b461f4f32d4e1960ef3c82b
Leonardo Ferreira <leonardoferreira172001@gmail.com>
2025-02-10 23:21:52 -0300

    Adiciona o readme com execuções

---
+++ README.md
@@ -0,0 +1,51 @@
+# Projeto de Sistemas Operacionais - PWM
+
+Este projeto controla um **servo motor** e um **LED** usando **PWM** no Raspberry Pi Pico. O servo se move entre 0° e 180° suavemente, e o LED varia seu brilho usando PWM. 
+
+## Descrição do projeto
+
+O objetivo deste projeto é configurar a GPIO **22** para controlar um **servo motor** e a GPIO **12** para controlar um **LED**. A movimentação do servo é realizada de forma suave entre **0°, 90° e 180°**. O LED possui um controle de intensidade baseado em **PWM**.
+
+## Execução do programa
+
+Execução do programa usando servo motor:
+
+![gifA](assets/GIF_SERVO_PWM.gif)
+
+Execução do programa usando LED RGB:
+![gifA](assets/GIF_LED_PWM.gif)
+
+## Requisitos
+
+Para rodar este projeto, você precisará de:
+- **Raspberry Pi Pico**
+- **Servo Motor**
+- **LED RGB**
+- **Software para compilação do Pico SDK**
+- **CMake e Make**
+- **Plugin do Raspberry Pi Pico no Visual Studio Code**
+- **Simulador Wokwi no Visual Studio Code**
+
+## Funcionalidades
+**Movimentação do Servo Motor**
+   - Posições fixas: **0°, 90°, 180°**
+   - Movimentação suave entre **0° ↔ 180°**  
+   
+**Controle do LED com PWM**
+   - LED altera o brilho de **0% a 100%**  
+   
+**Compatível com Simulação**
+   - Pode ser testado no **Wokwi** antes de rodar no hardware  
+
+## Como compilar e executar
+1. Configure o PICO-SDK no seu ambiente de desenvolvimento usando a extensão Raspberry Pi Pico no Visual Studio Code.
+2. Abra o arquivo correspondente no Visual Studio Code.
+3. Compile o firmware usando o botão de compilação no Visual Studio Code.
+4. Conecte a placa BitDogLab ao computador no modo BOOTSEL.
+5. Clique no botão denominado "run" no Visual Studio Code para carregar o firmware na placa.
+6. A placa executará automaticamente o firmware.
+
+## Autor
+Leonardo Ferreira Lima
+
+Email: ferreiiratech@gmail.com
--- main.c
+++ main.c
@@ -3,12 +3,12 @@
 #include "hardware/pwm.h"
 
 #define GPIO_PIN_SERVO       22     // Pino do servo
-#define GPIO_PIN_LED         12     // Pino do servo
+#define GPIO_PIN_LED         12     // Pino do led
 #define PWM_DIVISER          125.0  // Divisor de clock para 50 Hz
 #define WRAP_PERIOD          20000  // Período de 20 ms (50 Hz)
-#define POSITION_SERVO_180   2400   // Posição de 180 graus (2,4 ms)
-#define POSITION_SERVO_90    1470   // Posição de 90 graus (1,47 ms)
-#define POSITION_SERVO_0     500    // Posição de 0 graus (0,5 ms)
+#define POSITION_SERVO_180   2400   // Posição de 180 graus
+#define POSITION_SERVO_90    1470   // Posição de 90 graus
+#define POSITION_SERVO_0     500    // Posição de 0 graus
 #define TIME_DELAY_5000      5000   // Tempo de espera entre movimentos
 #define TIME_DELAY_10        10     // Tempo de espera entre movimentos
 #define STEP                 5      // Passo do movimento suave
@@ -57,22 +57,22 @@ int main() {
     init_pin_pwm_config(GPIO_PIN_LED, PWM_DIVISER, WRAP_PERIOD);
 
     // Move o servo para 180°
-    pwm_set_gpio_level(GPIO_PIN_SERVO, (POSITION_SERVO_180 * WRAP_PERIOD) / 20000);
+    pwm_set_gpio_level(GPIO_PIN_LED, (POSITION_SERVO_180 * WRAP_PERIOD) / 20000);
     sleep_ms(TIME_DELAY_5000);
 
     // Move o servo para 90°
-    pwm_set_gpio_level(GPIO_PIN_SERVO, (POSITION_SERVO_90 * WRAP_PERIOD) / 20000);
+    pwm_set_gpio_level(GPIO_PIN_LED, (POSITION_SERVO_90 * WRAP_PERIOD) / 20000);
     sleep_ms(TIME_DELAY_5000);
 
     // Move o servo para 0°
-    pwm_set_gpio_level(GPIO_PIN_SERVO, (POSITION_SERVO_0 * WRAP_PERIOD) / 20000);
+    pwm_set_gpio_level(GPIO_PIN_LED, (POSITION_SERVO_0 * WRAP_PERIOD) / 20000);
     sleep_ms(TIME_DELAY_5000);
 
     while (true) {
         // Movimenta o servo suavemente entre 0° e 180°
         // Pode ser modificado para adicionar o pino do led e fazer o led piscar
-        move_smoothly(GPIO_PIN_SERVO, POSITION_SERVO_0, POSITION_SERVO_180, STEP, TIME_DELAY_10);
-        move_smoothly(GPIO_PIN_SERVO, POSITION_SERVO_180, POSITION_SERVO_0, STEP, TIME_DELAY_10);
+        move_smoothly(GPIO_PIN_LED, POSITION_SERVO_0, POSITION_SERVO_180, STEP, TIME_DELAY_10);
+        move_smoothly(GPIO_PIN_LED, POSITION_SERVO_180, POSITION_SERVO_0, STEP, TIME_DELAY_10);
     }
 
     return 0;
